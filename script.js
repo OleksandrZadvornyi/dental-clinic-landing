24a05b4ebbbdf15353a2c040afcb724f337698bc
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2023-08-08 20:38:30 +0300

    add css for first four sections in main part

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function displayTopNav() {
   }
 }
 
+function myFunc() {
+  const h2 = document.getElementById("h");
+  const p = document.getElementById("p");
+
+  const h2Lines = Math.floor(h2.clientHeight / 64);
+  const pLines = Math.floor(p.clientHeight / 34);
+
+  p.style.marginBottom =
+    "calc(6.65rem + " + (h2Lines - 2) * 64 + "px - " + (pLines - 3) * 34 + "px)";
+}
+
+window.onload = myFunc();
+window.addEventListener("resize", myFunc);
