0c4e99c441595679d64520afd0eb61cb6f2e07f3
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2023-08-11 14:44:34 +0300

    add animation on scroll

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ function myFunc() {
 
 window.onload = myFunc();
 window.addEventListener("resize", myFunc);
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  }); 
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
